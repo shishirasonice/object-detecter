944bd98ad1cce495e494cfca4154a6b38ed0c8de
shishirasonice <82163635+shishirasonice@users.noreply.github.com>
2022-06-07 16:51:25 +0530

    Add files via upload

--- phone.js
+++ phone.js
@@ -21,6 +21,24 @@ function gotResult(error, results) {
 
 function draw(){
     image(phone_image, 0, 0, 450, 350);
+
+    if(status != ""){
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+            document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : "+ objects.length;
+
+   
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x - 15, objects[i].y - 15);
+  
+  
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x - 14, objects[i].y - 175, objects[i].width - 2326, objects[i].height - 2850);          }
+
+    }
+  
 }
 
 function modelLoaded() {
--- wardrobe.js
+++ wardrobe.js
@@ -17,10 +17,29 @@ function gotResult(error, results) {
       console.log(error);
     }
     console.log(results);
+    object = results;
 }
 
 function draw(){
     image(wardrobe_image, 0, 0, 350, 350);
+
+    if(status != ""){
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+            document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : "+ objects.length;
+
+   
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x - 15, objects[i].y - 15);
+  
+  
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x - 14, objects[i].y - 175, objects[i].width - 2326, objects[i].height - 2850);          }
+
+    }
+    
 }
 
 function modelLoaded() {
